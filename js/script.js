7337e4a980791633ad6ec0afd95d906cdfe7a948
Gaëtan <jerphagnon.gaetan@gmail.com>
2020-08-10 00:28:20 +0200

    broken game feature added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+let difficulty = 'Impossible';
+difficulties = {
+    'Easy':         2000,
+    'Medium':       1100,
+    'Hard':          800,
+    'Impossible':    1000
+};
+let pointCounter = 0;
+let difficultyTimer = 0;
+let broken = false;
+
 let game = {
     init: function(){
         console.log('Js Chargé')
@@ -6,23 +17,64 @@ let game = {
 
         let startButton = document.getElementById('start-button');
         startButton.addEventListener('click', game.startGame);
-        let difficulty = 'Medium';
+        let sizeForm = document.getElementById("size-container");
+        sizeForm.addEventListener('submit', game.changeGridSizeClickHandle);
+        game.changeGridSize(1);
         game.setDifficulty(difficulty);
-        
+        document.getElementById('grid-size-display').textContent= "5x5";
     },
-
-    generateCells: function(){
+    
+    changeGridSizeClickHandle: async function(event){
+        event.preventDefault();
+        let input = document.getElementById('size-input');
+        let number = input.value;
+        number = parseInt(number);
+        if (number){
+            if (number > 20 || number < 1) {
+                input.style.border ='2px solid red';
+                input.value = '';
+                if(number > 20){
+                    input.setAttribute('placeholder', 'Max 20!');
+                } else if (number < 1){
+                    input.setAttribute('placeholder', 'Min 1...');
+                }
+            } else {
+                input.value = '';
+                game.changeGridSize(number);
+                game.setColors();
+            }
+        }
+        input.value = '';
+        await sleep(1000);
+        input.style.border ='1px solid black';
+        input.setAttribute('placeholder', '1> ... >20');
+        let sizeForm = document.getElementById("size-container");
+        sizeForm.addEventListener('submit', game.changeGridSizeClickHandle);
+    },
+    
+    changeGridSize: function (number = 5){
         let grid = document.querySelector('#grid-container .grid');
-        console.log(grid);
-        for (let i=0; i<5 ; i++) {
+        while (grid.lastElementChild) {
+            grid.removeChild(grid.lastElementChild);
+        };
+        game.displayGridSize(number);
+        game.generateCells(number);
+    },
+    
+    displayGridSize: function(number){
+        let sizeDisplay = document.getElementById('grid-size-display');
+        sizeDisplay.textContent= number+'x'+number;
+    },
+    
+    generateCells: function(number = 5){
+        let grid = document.querySelector('#grid-container .grid');
+        for (let i=0; i<number ; i++) {
             let row = document.createElement('div');
             row.classList.add('row');
-            console.log(row);
-            for (let i2=0; i2<5 ; i2++){
+            for (let i2=0; i2<number ; i2++){
                 let cell = document.createElement('div');
                 cell.classList.add('cell');
                 row.appendChild(cell);
-                console.log(cell);
             }
             grid.appendChild(row);
         }
@@ -30,61 +82,50 @@ let game = {
     
     setDifficulty: function(mode) {
         let display = document.getElementById('difficulty-display');
-        let colorSet =[]
         
         for(d in difficulties) {
             if (mode == d) {
                 difficulty = d;
-                difficultyTimer = difficulties[d].t;
-                for (color in difficulties[d]){
-                    colorSet.push(difficulties[d][color]);
-                }
-                colorSet.splice(8,1);
-               game.setColors(colorSet);
+                difficultyTimer = difficulties[d];
             }
         }
+        game.setColors();
         display.textContent = difficulty;
         display.classList ="";
         display.classList.add('dif-'+difficulty.toLowerCase());
     },
-
+    
     setColors: function(){
         let container = document.getElementById('container');
         container.classList = '';
         container.classList.add('container-'+difficulty.toLowerCase());
-        console.log(container);
-
+        
         let infos = document.querySelector('.infos');
         infos.classList = '';
         infos.classList.add('infos');
         infos.classList.add('infos-'+difficulty.toLowerCase());
-        console.log(infos);
-
+        
         let scores = document.querySelector('.scores');
         scores.classList = '';
         scores.classList.add('scores');
         scores.classList.add('scores-'+difficulty.toLowerCase());
-        console.log(scores);
-
+        
         let difChoose = document.getElementById('difficulty-choice');
         difChoose.classList = '';
         difChoose.classList.add('difficulty-container-'+difficulty.toLowerCase());
-        console.log(difChoose);
-
+        
         let startButton = document.getElementById('start-button');
         startButton.classList = '';
         startButton.classList.add('start-button-'+difficulty.toLowerCase());
-        console.log(startButton);
-
+        
         let cells = document.querySelectorAll('.cell');
         cells.forEach(cell => cell.classList = '');
         cells.forEach(cell => cell.classList.add('cell'));
         cells.forEach(cell => cell.classList.add('cell-'+difficulty.toLowerCase()));
-        console.log(cells);
-
+        
     },
-
-
+    
+    
     chooseDifficultyHandle: function(event){
         let target = event.currentTarget;
         let targetParent = target.parentElement;
@@ -94,8 +135,8 @@ let game = {
         
         i.classList.remove("fa-chevron-right");
         i.classList.add("fa-chevron-down");
-    
-
+        
+        
         for(let difficulty in difficulties) {
             let option = document.createElement('li');
             let name = difficulty;
@@ -104,7 +145,7 @@ let game = {
             let lowerName = name.toLowerCase();
             option.classList.add("dif-"+lowerName);
             options.appendChild(option);
-          }
+        }
         
         target.removeEventListener("click", game.chooseDifficultyHandle)
         target.addEventListener('click', game.chooseDifficultyExit);
@@ -125,63 +166,72 @@ let game = {
         compareTarget.addEventListener("click", game.chooseDifficultyHandle);
         i.classList.remove("fa-chevron-down");
         i.classList.add("fa-chevron-right");
-        return;
     },
     
     removeDifficultyList: function(){
         let options = document.getElementById('options');
         while (options.lastElementChild) {
-        options.removeChild(options.lastElementChild);
+            options.removeChild(options.lastElementChild);
         };
         options.parentNode.removeChild(options);
         
-},
-
-getRandomCell: async function(){
-    let cellNumber = Math.floor((Math.random() * 25) + 1);
-        let cell = 0;
-        if (cellNumber <= 5) {
-
-            let row = document.querySelector('.grid .row:nth-child(1)').childNodes;
-            cell = row[cellNumber-1];
-        } else if (cellNumber > 5 && cellNumber <=10 ) {
-            cellNumber -= 5;
-            let row = document.querySelector('.grid .row:nth-child(2)').childNodes;
-            cell = row[cellNumber-1];
-        } else if (cellNumber > 10 && cellNumber <=15 ) {
-            cellNumber -= 10;
-            let row = document.querySelector('.grid .row:nth-child(3)').childNodes;
-            cell = row[cellNumber-1];
-        } else if (cellNumber > 15 && cellNumber <=20 ) {
-            cellNumber -= 15;
-            let row = document.querySelector('.grid .row:nth-child(4)').childNodes;
-            cell = row[cellNumber-1];
-        } else if (cellNumber > 20 && cellNumber <=25 ) {
-            cellNumber -= 20;
-            let row = document.querySelector('.grid .row:nth-child(5)').childNodes;
-            cell = row[cellNumber-1];
-        }
-
+    },
+    
+    getRandomCell: async function(){
+        let grid = document.querySelector('#grid-container .grid');
+        let gridSize = grid.childElementCount;
+        let cellNumber = Math.floor((Math.random() * gridSize*gridSize) + 0);
+        let index = Math.floor(cellNumber/gridSize);
+        console.log(cellNumber);
+        console.log(index);
+        let cellIndex = cellNumber-index*gridSize;
+        let rowIndex = index+1 ;
+        let row = document.querySelector('.grid .row:nth-child('+rowIndex+')').childNodes;
+        let cell = row[cellIndex];
+        console.log(row);
+        console.log(cell);
         cell.classList.add("target");
         cell.addEventListener('click', game.cellClickHandle)
         await sleep(difficultyTimer*0.75);
         cell.removeEventListener('click', game.cellClickHandle)
         cell.classList.remove("target");
         cell.classList.remove("target-valid");
+        if (difficulty == "Impossible") {
+            cell.classList.add("target-impossible");
+        }
     },
-
+    
     addPoints: function (points){
         pointCounter += points;
         let scoreDisplay = document.getElementById('score-display');
         scoreDisplay.textContent = pointCounter;
     },
-
-    cellClickHandle: function (event){
+    
+    cellClickHandle: async function (event){
         let target = event.currentTarget;
         game.addPoints(1/difficultyTimer*100000);
         target.removeEventListener('click', game.cellClickHandle);
         target.classList.remove("target");
         target.classList.add("target-valid");
+        if (target.classList.contains("target-impossible")){
+            target.classList.add("target-crack");
+            target.classList.remove("target-impossible");
+            target.classList.remove("cell");
+            target.classList.remove("target-valid");
+            target.classList.remove("target");
+            target.style.transition = "0";
+            let posX = event.screenX;
+            let posY = event.clientY;
+            let x = target.offsetLeft;
+            let y = target.offsetTop;
+            let w = target.offsetWidth;
+            let h = target.offsetHeight;
+            let bgx = posX-x-w/1.6;
+            let bgy = posY-y-h/1.8;
+
+            target.style.backgroundPosition = bgx+"px "+bgy+"px";
+            broken = true;
+        }     
     },
 
     startGame: async function(event){
@@ -192,27 +242,38 @@ getRandomCell: async function(){
         difficultyButton.style.backgroundColor ="grey";
         let button = event.currentTarget;
         button.removeEventListener('click', game.startGame);
+        let sizeForm = document.getElementById("size-container");
+        sizeForm.removeEventListener('submit', game.changeGridSizeClickHandle)
         let buttonColor = button.style.backgroundColor;
         button.style.backgroundColor ="grey";
-
+        
         let screen = document.querySelector('.screen p');
         screen.textContent ='Game starting...';
         progress(difficultyTimer/100);
         await sleep(difficultyTimer*1.1);
         screen.textContent ='Game in progress...';
-
-        for (let i=0; i<10 ; i++) {
-            game.getRandomCell();
-            await sleep(difficultyTimer);
+        
+        for (let i=0; i<15 ; i++) {
+            if(broken == false){
+                game.getRandomCell();
+                await sleep(difficultyTimer);
+            } else if (broken) {
+                return alert('You clicked too hard you broke my game!');
+            }
         }
-
+        
         screen.textContent = '';
         difficultyButton.addEventListener('click', game.chooseDifficultyHandle);
+        sizeForm.addEventListener('submit', game.changeGridSizeClickHandle)
         difficultyButton.style.backgroundColor = buttonColor;
         button.style.backgroundColor = buttonColor;
         button.addEventListener('click', game.startGame);
     },
 
+    stopGame: function(){
+
+    },
+    
 };
 
 function sleep(ms) {
@@ -221,30 +282,20 @@ function sleep(ms) {
 
 let i = 0;
 function progress(ms) {
-  if (i == 0) {
-    i = 1;
-    var elem = document.getElementById("myBar");
-    var width = 1;
-    var id = setInterval(frame, ms);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        width++;
-        elem.style.width = width + "%";
-      }
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("myBar");
+        var width = 1;
+        var id = setInterval(frame, ms);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
     }
-  }
 }
-
-difficulties = {
-    'Easy':         {'c1': '#007304', 'c2': '#4ae440', 'c3': '#29a027', 'c4': '#73d076', 'c5': '#016e09', 'c6': '#7deb8b', 'c7': '#24b522', 'c8': '#b9deb0','t': 2000},
-    'Medium':       {'c1': '#002e73', 'c2': '#4095e4', 'c3': '#275ca0', 'c4': '#73a3d0', 'c5': '#01236e', 'c6': '#7daaeb', 'c7': 'royalblue', 'c8': 'lightsteelblue','t': 1100},
-    'Hard':         {'c1': '#730000', 'c2': '#e44040', 'c3': '#a02727', 'c4': '#d07373', 'c5': '#6e0101', 'c6': '#eb7d7d', 'c7': '#b52222', 'c8': '#deb0b0','t': 800},
-    'Impossible':   {'c1': '#151515', 'c2': '#737373', 'c3': '#4e4e4e', 'c4': '#adadad', 'c5': '#333333', 'c6': '#cac9c9', 'c7': '#464646', 'c8': '#d0d0d0','t': 250}
-};
-let pointCounter = 0;
-let difficultyTimer = 0;
-game.generateCells();
 game.init();
